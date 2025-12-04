eb3bb2df4cc3eae4f2d5819b84d5950aeb918f1f
Alyssa Hollingshead <aly.hollingshead@gmail.com>
2025-12-04 10:28:12 -0600

    chore: add primevue ui

---
+++ src/config.primevue.ts
@@ -0,0 +1,65 @@
+import { definePreset } from '@primeuix/themes'
+import Aura from '@primeuix/themes/dist/aura'
+
+import type { PrimeVueConfiguration } from 'primevue'
+
+/**
+ * Customization keys for Aura can be found at
+ * https://github.com/primefaces/primeuix/blob/main/packages/themes/src/presets/aura/base/index.ts
+ */
+
+const prpThemePreset = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '{cyan.50}',
+      100: '{cyan.100}',
+      200: '{cyan.200}',
+      300: '{cyan.300}',
+      400: '{cyan.400}',
+      500: '{cyan.500}',
+      600: '{cyan.600}',
+      700: '{cyan.700}',
+      800: '{cyan.800}',
+      900: '{cyan.900}',
+      950: '{cyan.950}',
+    },
+    colorScheme: {
+      dark: {
+        surface: {
+          0: '#ffffff',
+          50: '{slate.50}',
+          100: '{slate.100}',
+          200: '{slate.200}',
+          300: '{slate.300}',
+          400: '{slate.400}',
+          500: '{slate.500}',
+          600: '{slate.600}',
+          700: '{slate.700}',
+          800: '{slate.800}',
+          900: '{slate.900}',
+          950: '{slate.950}',
+        },
+        formField: {
+          // background: '#0002',
+          background: '{slate.950}',
+          disabledBackground: '{slate.900}',
+          // borderColor: '#fff1',
+          borderColor: '{slate.700}',
+          hoverBorderColor: '{slate.600}',
+          focusBorderColor: '{primary.700}',
+        },
+      },
+    },
+  },
+})
+
+const primeVueConfiguration: PrimeVueConfiguration = {
+  theme: {
+    preset: prpThemePreset,
+    options: {
+      darkModeSelector: true,
+    },
+  },
+}
+
+export default primeVueConfiguration
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
+import router from './router'
+import primeVueConfiguration from './config.primevue'
 
 import App from './App.vue'
-import router from './router'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, primeVueConfiguration)
 
 app.mount('#app')
